cd5975f0b7d90e1717e42e41a61749173fb15840
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2020-12-02 17:09:23 +0900

    コントローラーとマイグレーションファイルの編集

--- db/migrate/20201202064725_devise_create_users.rb
+++ db/migrate/20201202064725_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.date :birthday
 
       ## Recoverable
       t.string   :reset_password_token
