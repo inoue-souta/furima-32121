7cde6ba04dcc3fd7bfee053996d15c67cc7f55f0
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2020-12-08 19:18:13 +0900

    テストコード修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,27 +24,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Explanation can't be blank"
       end
       it 'category_idが1では保存できない' do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category must be other than 1"
       end
       it 'status_idが1では保存できない' do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Status must be other than 1"
       end
       it 'burden_idが1では保存できない' do
-        @item.burden_id = "1"
+        @item.burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Burden must be other than 1"
       end
       it 'prefecture_idが1では保存できない' do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture must be other than 1"
       end
       it 'day_idが1では保存できない' do
-        @item.day_id = "1"
+        @item.day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Day must be other than 1"
       end
