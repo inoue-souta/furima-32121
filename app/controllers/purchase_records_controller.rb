7c407714a2c5a1ae1f2415b898eee107f840e05d
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2020-12-15 12:21:01 +0900

    購入機能　クレジット決済機能の実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -5,8 +5,10 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new(purchase_params)
     if @purchase.valid?
+      pay_item
       @purchase.save
       redirect_to root_path
     else
@@ -17,7 +19,16 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:item_id, :user_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :purchase_record_id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :purchase_record_id).merge(token: params[:token],item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,8 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :purchase_record_id
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :purchase_record_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
@@ -11,7 +12,7 @@ class PurchaseAddress
   end
 
   def save
-    Purchase_record.create(item_id: item_id, user_id: user_id)
-    Street_address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number, purchase_record_id: purchase_record_id)
+    purchaserecord = PurchaseRecord.create(item_id: item_id, user_id: user_id) #purchaserecord (id: 1 , item_id: 2 ,user_id: 3)
+    StreetAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number, purchase_record_id: purchaserecord.id)#purchaserecord.id
   end
 end
